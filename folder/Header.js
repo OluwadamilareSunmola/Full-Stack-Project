1683097a84374b3fcbb2e824be58f38ee5a70a25
Oluwadamilare Sunmola <144754597+OluwadamilareSunmola@users.noreply.github.com>
2024-01-12 00:15:41 -0600

    Add files via upload

---
+++ folder/Header.js
@@ -0,0 +1,13 @@
+import './header.css'
+
+const Header = () => {
+    return {
+        <div className = 'header'>
+        <h1 className = 'logo'>Movie Picker</h1>
+        <div className = 'links'>
+
+        </div>
+    }
+}
+
+export default Header
